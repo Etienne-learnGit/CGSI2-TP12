acf6a14e6a52d114d574340cfe24f082d4578380
Etienne-learnGit <etienne.etudes@gmail.com>
2020-03-31 12:16:04 +0200

    TP12

--- Exercice 2.py
+++ Exercice 2.py
@@ -1,9 +1,8 @@
 def reverseString(s, i = 0) :
     """Reverses the string s recursively """
     if i == len(s):
-        return ""
-    return reverseString(s, i+1) ...
-
+        return s[::-1]
+    return reverseString(s, i+1)
 
 print(reverseString("")) # ""
 print(reverseString("bonjour")) # ruojnob
